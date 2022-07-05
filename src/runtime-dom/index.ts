55991ed9b70f756bc6a18b2ebbeafcf1269a091c
vincyda <1322654206@qq.com>
2022-07-05 15:28:59 +0800

    更新element 的 props

---
+++ src/runtime-dom/index.ts
@@ -0,0 +1,35 @@
+import { createRenderer } from "../runtime-core";
+
+function createElement(type) {
+  return document.createElement(type);
+}
+
+function patchProp(el, key, prevVal, nextVal) {
+  const isOn = (key: string) => /^on[A-Z]/.test(key);
+  if (isOn(key)) {
+    const event = key.slice(2).toLowerCase();
+    el.addEventListener(event, nextVal);
+  } else {
+    if (nextVal === undefined || nextVal === null) {
+      el.removeAttribute(key);
+    } else {
+      el.setAttribute(key, nextVal);
+    }
+  }
+}
+
+function insert(el, parent) {
+  parent.append(el);
+}
+
+const renderer: any = createRenderer({
+  createElement,
+  patchProp,
+  insert,
+});
+
+export function createApp(...args) {
+  return renderer.createApp(...args);
+}
+
+export * from "../runtime-core";
